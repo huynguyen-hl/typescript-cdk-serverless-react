423a9554fc5f2ef9d02e6d081016000b680ff8e4
Huy Nguyen <scythedead.h@gmail.com>
2025-07-06 17:20:19 +0700

    fix: correct response message in hello function test

--- cdk-cicd/test/cdk-cicd.test.ts
+++ cdk-cicd/test/cdk-cicd.test.ts
@@ -3,7 +3,8 @@ import { handler } from '../services/hello';
 describe('hello function', () => {
   it('should return status code 200', () => {
     const result = handler({}, {});
+
     expect(result.statusCode).toEqual(200);
-    expect(result.body).toEqual(JSON.stringify('Hello from Lambda!!'));
+    expect(result.body).toEqual(JSON.stringify('Hello from Lambda!'));
   });
 });

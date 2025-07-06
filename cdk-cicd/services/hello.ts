a4433eae1ea41db0eab41963679d786f2dadfa93
Huy Nguyen <scythedead.h@gmail.com>
2025-07-06 15:39:26 +0700

    update hello function response message and corresponding test case

--- cdk-cicd/services/hello.ts
+++ cdk-cicd/services/hello.ts
@@ -1,7 +1,7 @@
 function handler(event: any, context: any, callback: Function) {
   return callback(null, {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!')
+    body: JSON.stringify('Hello from Lambda!!')
   })
 }
 
--- cdk-cicd/test/cdk-cicd.test.ts
+++ cdk-cicd/test/cdk-cicd.test.ts
@@ -2,8 +2,9 @@ import { handler } from '../services/hello';
 
 describe('hello function', () => {
   it('should return status code 200', () => {
-    const result = handler({}, {}, (error: any, response: any) => {
+    handler({}, {}, (error: any, response: any) => {
       expect(response.statusCode).toEqual(200);
+      expect(response.body).toEqual(JSON.stringify('Hello from Lambda!!'));
     });
   });
 });
